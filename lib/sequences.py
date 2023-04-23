8d27a1470d49a6f03939ad5e69976def89fddd4c
James Aidan Zimmermann <zimmermann.lowbrass@gmail.com>
2023-04-22 19:43:39 -0500

    completed labwork

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,17 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+    fib_list = []
+    while length > 0:
+
+        if len(fib_list) == 0 :
+            fib_list.append(0)
+        elif len(fib_list) == 1:
+            fib_list.append(1)
+        else:
+            x = fib_list[len(fib_list) - 1]
+            y = fib_list[len(fib_list) - 2]
+            fib_list.append(x + y)
+        length -= 1
+
+    print(fib_list)
